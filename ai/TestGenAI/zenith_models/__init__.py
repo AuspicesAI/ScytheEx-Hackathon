71bb16fcc80d1e1c50c7108d387f119eaa05701c
smadi0x86 <smadixd@gmail.com>
2024-04-19 18:00:15 +0300

    Init, new project

--- ai/TestGenAI/zenith_models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .code_analysis import CodeAnalysisModel
-from .test_generation import TestGenerationModel
-from .code_suggestion import CodeSuggestionModel
-
-__all__ = ["CodeAnalysisModel", "TestGenerationModel", "CodeSuggestionModel"]
-__version__ = "1.0.0"
