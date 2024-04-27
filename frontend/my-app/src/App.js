05f7af7585d01e9824f0268cd98e2204cc2c1e6c
a7madprog <ahmadraed756@gmail.com>
2024-04-27 18:00:36 +0300

    latest_front

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Chart from './Chart';
+
+function App() {
+  return (
+    <div className="App">
+     <Chart/>
+    </div>
+  );
+}
+
+export default App;
