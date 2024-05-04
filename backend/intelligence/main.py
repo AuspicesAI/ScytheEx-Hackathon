0c0e7bb8ff7036444451b750f26b204179630720
smadi0x86 <smadixd@gmail.com>
2024-05-05 00:45:33 +0300

    Added copyrights

--- backend/intelligence/main.py
+++ backend/intelligence/main.py
@@ -1,3 +1,19 @@
+# Copyright (c) 2024 AuspicesAI
+#
+# This file is part of ScytheEx.
+#
+# ScytheEx is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License 2.0 as published by
+# the Apache Software Foundation, either version 2 of the License, or any later version.
+#
+# ScytheEx is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Apache License 2.0 for more details.
+#
+# You should have received a copy of the Apache License 2.0
+# along with ScytheEx. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
+
 import requests
 import redis
 import json
